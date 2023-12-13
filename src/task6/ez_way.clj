4174e33c466ec9a1650f2a9036f74c1b5cdcad37
Mikhail Oskilko <311707@niuitmo.ru>
2023-12-13 10:58:24 +0300

    fix warnings in ez_Way

--- src/task6/ez_way.clj
+++ src/task6/ez_way.clj
@@ -1,22 +1,12 @@
 (ns task6.ez-way)
 
+(defn find_sum_squares [y]
+  (/ (apply * [y (+ y 1) (+ (* 2 y) 1)]) 6))
+
+(defn find_squares_sum [y]
+  (Math/pow (/ (* y (+ y 1)) 2) 2))
+
 (defn find_diff_ez [n]
-  (defn find_sum_squares [y]
-    (/ (apply * [y (+ y 1) (+ (* 2 y) 1)]) 6))
-
-  (defn find_squares_sum [y]
-    (Math/pow (/ (* y (+ y 1)) 2) 2))
-
-  ;;  (println (find_squares_sum n))
-  ;;(println (find_sum_squares n))
   (- (find_squares_sum n) (find_sum_squares n)))
 
-  
 (find_diff_ez 100)
-
-
-  
-  
-
-  
-
